3b2dc7496ebeb972ca7e54f13b5ec1db7aaf9d53
David Coulombe <dlcoulombe@gmail.com>
2019-04-11 14:59:12 -0400

    Admin v1.2
    On s'amuse avec la base de données!

--- thot_eLearning/thot_eLearning/Controllers/HomeController.cs
+++ thot_eLearning/thot_eLearning/Controllers/HomeController.cs
@@ -16,12 +16,43 @@ namespace thot_eLearning.Controllers
             var cours = from c in bd.Cours
                         select c;
 
+            
+
+
             ViewBag.title = "Admin";
             ViewBag.message = "Voici la liste des cours";
 
-            return View(cours);
-        }
 
+            return View(cours);
+
+        }
+        [HttpPost]
+        public ActionResult Admin(string Nom, string Description, string Prerequis)
+        {
+            DataClassAdminDataContext bd = new DataClassAdminDataContext();
+            var cmd = Request["cmd"].ToString();
+            if (cmd == "1")
+            {
+                Cour insertion = new Cour
+                {
+                    Nom = Request["nom"].ToString(),
+                    Description = Request["description"].ToString(),
+                    Prerequis = Request["prerequis"].ToString()
+                };
+                bd.Cours.InsertOnSubmit(insertion);
+                try
+                {
+                    bd.SubmitChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    bd.SubmitChanges();
+                }
+            }
+            return View();
+        }
+       
         
     }
 }
---
+++ thot_eLearning/thot_eLearning/Scripts/ModifDataBase.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    function insert(nom, description, prerequis) {
+        nom = document.getElementById("nom").value;
+        description = document.getElementById("description").value;
+        prerequis = document.getElementById("prerequis").value;
+
+        $.post("~/Controllers/HomeController.cs", { nom = nom, description = description, prerequis = prerequis, cmd=1 } function (data) {
+
+        });
+    }
+});

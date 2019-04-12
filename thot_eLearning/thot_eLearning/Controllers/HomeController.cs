46043e5c1c6cce0be8f0e44e11c546a7e9c9c8c3
David Coulombe <dlcoulombe@gmail.com>
2019-04-12 00:35:37 -0400

    sadas

--- thot_eLearning/thot_eLearning/Controllers/HomeController.cs
+++ thot_eLearning/thot_eLearning/Controllers/HomeController.cs
@@ -13,10 +13,51 @@ namespace thot_eLearning.Controllers
             return View();
         }
 
+<<<<<<< HEAD
         public ActionResult About()
+=======
+        /// <summary>
+        /// Main page. You can see all the classes listed in in order in which they were inserted into the Database
+        /// Many more options such as editing, updating, deleting and adding.
+        /// </summary>
+        /// <returns> The return simply returns a list of all the classes</returns>
+        public ActionResult Admin()
+        {
+            IList<Cours> uneListe = new List<Cours>();
+
+            var query = from c in context.Cours
+                        select c;
+            var publish = query.ToList();
+            foreach(var data in publish)
+            {
+                uneListe.Add(new Cours()
+                {
+                    Nom = data.Nom,
+                    Description = data.Description,
+                    Prerequis = data.Prerequis
+                });
+            }
+            return View(uneListe);
+
+        }
+        /// <summary>
+        /// Initialise the Insert() function
+        /// It will simple insert into the context variable which we created earlier.
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Insert()
+        {
+            Cours model = new Cours();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Insert(Cours model)
+>>>>>>> parent of 3125e6f... Admin v.3
         {
             ViewBag.Message = "Your application description page.";
 
+<<<<<<< HEAD
             return View();
         }
 
@@ -26,5 +67,28 @@ namespace thot_eLearning.Controllers
 
             return View();
         }
+=======
+                Cour insertion = new Cour
+                {
+                    Nom = model.Nom,
+                    Description = model.Description,
+                    Prerequis = model.Prerequis
+                };
+                context.Cours.InsertOnSubmit(insertion);
+                try
+                {
+                    context.SubmitChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    context.SubmitChanges();
+                }
+            
+            return View(model);
+        }
+       
+        
+>>>>>>> parent of 3125e6f... Admin v.3
     }
 }
---
+++ thot_eLearning/thot_eLearning/Models/Cours.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace thot_eLearning.Models
+{
+    public class Cours
+    {
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public string Prerequis { get; set; }
+    }
+}

bad781dcf30f8e4289058ca22bd3736a7b29182b
David Coulombe <dlcoulombe@gmail.com>
2019-04-12 00:48:04 -0400

    merge

--- thot_eLearning/thot_eLearning/Controllers/HomeController.cs
+++ thot_eLearning/thot_eLearning/Controllers/HomeController.cs
@@ -18,8 +18,11 @@ namespace thot_eLearning.Controllers
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         public ActionResult About()
 =======
+=======
+>>>>>>> parent of 3125e6f... Admin v.3
 =======
 >>>>>>> parent of 3125e6f... Admin v.3
 =======
@@ -105,6 +108,9 @@ namespace thot_eLearning.Controllers
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 3125e6f... Admin v.3
+=======
 >>>>>>> parent of 3125e6f... Admin v.3
 =======
 >>>>>>> parent of 3125e6f... Admin v.3

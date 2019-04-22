a502aad7c88fbb8160ccd38e0b89b23a6d731a21
Gabriel Mercier <gabriel.mercier96@gmail.com>
2019-04-22 13:26:46 -0400

    debut eleve

---
+++ thot_eLearning/thot_eLearning/Controllers/eleveController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using thot_eLearning.Models;
+
+namespace thot_eLearning.Controllers
+{
+    public class eleveController : Controller
+    {
+
+        private DataClassAdminDataContext context;
+
+        public eleveController()
+        {
+            context = new DataClassAdminDataContext();
+        }
+
+        // GET: eleve
+        public ActionResult Eleve()
+        {
+            IList<Cours> uneListe = new List<Cours>();
+            var query = from c in context.Cours
+                select c;
+            var querryRez = query.ToList();
+            foreach (var data in querryRez)
+            {
+                uneListe.Add(new Cours()
+                {
+                    Nom = data.Nom,
+                    Description = data.Description,
+                    Prerequis = data.Prerequis,
+                    NbModules = data.NbModules
+                });
+            }
+            return View(uneListe);
+        }
+    }
+}
